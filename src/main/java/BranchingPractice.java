230206ca0dd53d1ed81e7bbeb9a0dd7e37962be2
Uzma <uzmaahsan27@gmail.com>
2021-05-25 19:27:38 -0400

    Created new branch

--- src/main/java/BranchingPractice.java
+++ src/main/java/BranchingPractice.java
@@ -1,6 +1,8 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
-
+//create new project
+        //enabled version control
+        //add .getIgnore with idea
     }
 }
